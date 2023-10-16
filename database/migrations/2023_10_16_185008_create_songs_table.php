0289fa5f03cdcc11d53e79896992a1ee0192cb94
Sonam Datok <datoksonam@gmail.com>
2023-10-16 15:02:01 -0400

    add breeze scaffolding and song model

---
+++ database/migrations/2023_10_16_185008_create_songs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class); //FK for the user who posted the song
+            $table->integer('upvote');
+            $table->json('comments');
+            $table->string('songLink');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('songs');
+    }
+};
--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
+            input: 'resources/js/app.jsx',
             refresh: true,
         }),
         react(),
     ],
-    esbuild: {
-        jsxInject: `import React from 'react'`, // Add this line to inject React for JSX
-      },
 });
