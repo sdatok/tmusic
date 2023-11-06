b9d5da24025eca186122fa45883632c6c8601974
Sonam Datok <datoksonam@gmail.com>
2023-11-06 06:38:58 -0500

    Album Search Feature

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -77,7 +77,7 @@ export default function Login({ status, canResetPassword }) {
 
                     </label>
                 </div>
-                <div className="flex items-center justify-center">
+                <div className="mt-6 flex items-center justify-center">
                     <a href={route('google.redirect')}
                         className="px-4 py-2 border flex gap-2 border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150">
                         <img className="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg"
--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import {Head} from '@inertiajs/react';
 import {useEffect, useState} from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container, InputGroup, FormControl, Button, Row, Card} from 'react-bootstrap';
 
 const CLIENT_ID = "f447bf4cf4ea4195a01796a7e26dcba7";
@@ -12,8 +12,9 @@ const CLIENT_SECRET = "71ec81c0596242b88cf4d1c463903631";
 export default function Dashboard({auth}) {
     const [searchInput, setSearchInput] = useState("");
     const [accessToken, setAccessToken] = useState("");
+    const [albums, setAlbums] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         //API ACCESS TOKEN
         var authParameters = {
             method: 'POST',
@@ -26,6 +27,34 @@ export default function Dashboard({auth}) {
             .then(result => result.json())
             .then(data => setAccessToken(data.access_token))
     }, [])
+
+    //Search
+    async function search() {
+        console.log("Search for " + searchInput); //ie. Kanye West
+
+        //Get request using search to get Song title
+        var searchParameters = {
+            method: 'GET',
+            headers: {
+                'Content-type': 'application/json',
+                'Authorization': 'Bearer ' + accessToken
+            }
+        }
+        var artistID = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=artist', searchParameters)
+            .then(response => response.json())
+            .then(data => {return data.artists.items[0].id})
+
+        console.log("Artist ID is " + artistID);
+        //Get request with Artist ID grab all albums from that artist
+        var returnedAlbums = await fetch('https://api.spotify.com/v1/artists/' + artistID + '/albums' + '?include_groups=album&market=US&limit=50', searchParameters)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setAlbums(data.items);
+            });
+        //Display those albums
+    }
+
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -33,44 +62,76 @@ export default function Dashboard({auth}) {
         >
             <Head title="Dashboard"/>
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900 dark:text-gray-100">You're logged in!</div>
-                    </div>
-                </div>
-            </div>
-
-            <Container>
-                <InputGroup className="mb-3" size="1g">
-                    <FormControl
+            <div className="container mx-auto px-4 py-12">
+                <div className="mb-3 flex items-center">
+                    <input
+                        className="border-2 border-gray-300 w-1/2 p-2 mr-2"
                         placeholder="Search For Artist"
-                        type=" input"
+                        type="text"
                         onKeyPress={event => {
-                            if (event.key == "Enter") {
-                                console.log("Pressed enter");
+                            if (event.key === 'Enter') {
+                                search();
                             }
                         }}
                         onChange={event => setSearchInput(event.target.value)}
                     />
-                    <Button onClick={() => {
-                        console.log("Clicked button")
-                    }}>
+                    <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                        onClick={() => {
+                            search();
+                        }}
+                    >
                         Search
-                    </Button>
-                </InputGroup>
-            </Container>
-            <Container>
-                <Row className="mx-2 row row-cols-4">
-                    <Card>
-                        <Card.Img src="#"/>
-                        <Card.Body>
-                            <Card.Title>Album Name Here</Card.Title>
-                        </Card.Body>
-                    </Card>
-
-                </Row>
-            </Container>
+                    </button>
+                </div>
+            </div>
+            <div className="container mx-auto px-4">
+                <div className="grid grid-cols-4 gap-4 mx-2">
+                    {albums.map((album, i) => {
+                        return (
+                            <div className="max-w-xs rounded-xl overflow-hidden shadow-lg" key={i}>
+                                <img className="w-full" src={album.images[0].url} alt={album.name} />
+                                <div className="px-6 py-4">
+                                    <div className="font-bold text-white text-xl mb-2">{album.name}</div>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+            {/*<Container>*/}
+            {/*    <InputGroup className="mb-3" size="1g">*/}
+            {/*        <FormControl*/}
+            {/*            placeholder="Search For Artist"*/}
+            {/*            type=" input"*/}
+            {/*            onKeyPress={event => {*/}
+            {/*                if (event.key == "Enter") {*/}
+            {/*                    console.log("Pressed enter");*/}
+            {/*                }*/}
+            {/*            }}*/}
+            {/*            onChange={event => setSearchInput(event.target.value)}*/}
+            {/*        />*/}
+            {/*        <Button onClick={() => {*/}
+            {/*            console.log("Clicked button")*/}
+            {/*        }}>*/}
+            {/*            Search*/}
+            {/*        </Button>*/}
+            {/*    </InputGroup>*/}
+            {/*</Container>*/}
+            {/*<Container>*/}
+            {/*    <Row className="mx-2 row row-cols-4">*/}
+            {/*        {albums.map( (album, i)=> {*/}
+            {/*            return (*/}
+            {/*                <Card>*/}
+            {/*                    <Card.Img src="album.images[0].url"/>*/}
+            {/*                    <Card.Body>*/}
+            {/*                        <Card.Title>{album.name}</Card.Title>*/}
+            {/*                    </Card.Body>*/}
+            {/*                </Card>*/}
+            {/*            )*/}
+            {/*        })}*/}
+            {/*    </Row>*/}
+            {/*</Container>*/}
 
             {/*<div className="w-5 h5">*/}
             {/*<Foryou/>*/}
