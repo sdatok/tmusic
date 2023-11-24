fec74c1b08289c9b76463c374e7af61624cb07c1
Sonam Datok <datoksonam@gmail.com>
2023-11-23 21:14:51 -0500

    Install Chatify and adding it to Navbar (AuthenticatedLayout.jsx)

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -22,7 +22,10 @@ export default function Authenticated({ user, header, children }) {
 
                             <div className="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                                 <NavLink href={route('dashboard')} active={route().current('dashboard')}>
-                                    Dashboard
+                                    Post
+                                </NavLink>
+                                <NavLink href={route('chatify')} active={route().current('chatify')}>
+                                    Chat
                                 </NavLink>
                             </div>
                         </div>
